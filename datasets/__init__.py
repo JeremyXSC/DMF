78be849f86c8b3f7c73738eb805a3cdaf0da55a4
JeremyXSC <xiangsuncheng17@sjtu.edu.cn>
2022-10-26 19:38:01 +0800

    update

---
+++ datasets/__init__.py
@@ -0,0 +1,3 @@
+from .make_dataloader import make_dataloader, make_dataloader_cross
+
+
---
+++ loss/make_loss.py
@@ -0,0 +1,86 @@
+# encoding: utf-8
+"""
+@author:  liaoxingyu
+@contact: sherlockliao01@gmail.com
+"""
+import torch
+import torch.nn.functional as F
+from .softmax_loss import CrossEntropyLabelSmooth, LabelSmoothingCrossEntropy
+from .triplet_loss import TripletLoss
+from .center_loss import CenterLoss
+
+
+def make_loss(cfg, num_classes):    # modified by gu
+    sampler = cfg.DATALOADER.SAMPLER
+    feat_dim = 2048
+    center_criterion = CenterLoss(num_classes=num_classes, feat_dim=feat_dim, use_gpu=True)  # center loss
+    if 'triplet' in cfg.MODEL.METRIC_LOSS_TYPE:
+        if cfg.MODEL.NO_MARGIN:
+            triplet = TripletLoss()
+            print("using soft triplet loss for training")
+        else:
+            triplet = TripletLoss(cfg.SOLVER.MARGIN)  # triplet loss
+            print("using triplet loss with margin:{}".format(cfg.SOLVER.MARGIN))
+    else:
+        print('expected METRIC_LOSS_TYPE should be triplet'
+              'but got {}'.format(cfg.MODEL.METRIC_LOSS_TYPE))
+
+    if cfg.MODEL.IF_LABELSMOOTH == 'on':
+        xent = CrossEntropyLabelSmooth(num_classes=num_classes)
+        print("label smooth on, numclasses:", num_classes)
+
+    if sampler == 'softmax':
+        def loss_func(score, feat, target):
+            return F.cross_entropy(score, target)
+
+    elif cfg.DATALOADER.SAMPLER == 'softmax_triplet':
+        def loss_func(score, feat, target, featt):
+            if cfg.MODEL.METRIC_LOSS_TYPE == 'triplet':
+                N = feat.shape[0]
+                # label = torch.tensor([1 for _ in range(N)]).to('cuda')
+                similarity_loss = F.cosine_embedding_loss(feat, featt, target)
+                if cfg.MODEL.IF_LABELSMOOTH == 'on':
+                    if isinstance(score, list):
+                        ID_LOSS = [xent(scor, target) for scor in score[1:]]
+                        ID_LOSS = sum(ID_LOSS) / len(ID_LOSS)
+                        ID_LOSS = 0.5 * ID_LOSS + 0.5 * xent(score[0], target)
+                    else:
+                        ID_LOSS = xent(score, target)
+
+                    if isinstance(feat, list):
+                            TRI_LOSS = [triplet(feats, target)[0] for feats in feat[1:]]
+                            TRI_LOSS = sum(TRI_LOSS) / len(TRI_LOSS)
+                            TRI_LOSS = 0.5 * TRI_LOSS + 0.5 * triplet(feat[0], target)[0]
+                    else:
+                            TRI_LOSS = triplet(feat, target)[0]
+
+
+                    return cfg.MODEL.ID_LOSS_WEIGHT * ID_LOSS + \
+                               cfg.MODEL.TRIPLET_LOSS_WEIGHT * TRI_LOSS + 0.5*cfg.MODEL.ID_LOSS_WEIGHT *similarity_loss
+                else:
+                    if isinstance(score, list):
+                        ID_LOSS = [F.cross_entropy(scor, target) for scor in score[1:]]
+                        ID_LOSS = sum(ID_LOSS) / len(ID_LOSS)
+                        ID_LOSS = 0.5 * ID_LOSS + 0.5 * F.cross_entropy(score[0], target)
+                    else:
+                        ID_LOSS = F.cross_entropy(score, target)
+
+                    if isinstance(feat, list):
+                            TRI_LOSS = [triplet(feats, target)[0] for feats in feat[1:]]
+                            TRI_LOSS = sum(TRI_LOSS) / len(TRI_LOSS)
+                            TRI_LOSS = 0.5 * TRI_LOSS + 0.5 * triplet(feat[0], target)[0]
+                    else:
+                            TRI_LOSS = triplet(feat, target)[0]
+
+                    return cfg.MODEL.ID_LOSS_WEIGHT * ID_LOSS + \
+                               cfg.MODEL.TRIPLET_LOSS_WEIGHT * TRI_LOSS + 0.5*cfg.MODEL.ID_LOSS_WEIGHT *similarity_loss
+            else:
+                print('expected METRIC_LOSS_TYPE should be triplet'
+                      'but got {}'.format(cfg.MODEL.METRIC_LOSS_TYPE))
+
+    else:
+        print('expected sampler should be softmax, triplet, softmax_triplet or softmax_triplet_center'
+              'but got {}'.format(cfg.DATALOADER.SAMPLER))
+    return loss_func, center_criterion
+
+
